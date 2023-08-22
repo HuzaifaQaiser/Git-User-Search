3c1250397e991afca85f8fd6f9bacbf3d1384ddc
Huzaifa Qaiser <huzaifaqaiser797@gmail.com>
2023-08-22 14:03:01 +0500

    First Commit

--- src/components/GitSearch.jsx
+++ src/components/GitSearch.jsx
@@ -76,12 +76,12 @@ function GitSearch() {
 
   const profilePicStyle = {
     alignSelf: "center",
-    width: "40%",
+    width: "20%",
   };
 
   // Add a media query for screens with a minimum width of 640px
   if (window.matchMedia("(min-width: 640px)").matches) {
-    parentDivStyle.flexDirection = "row";
+    parentDivStyle.flexDirection = "column";
     parentDivStyle.width = "80%";
     parentDivStyle.marginLeft = "10%";
   }
@@ -95,6 +95,7 @@ function GitSearch() {
         backgroundRepeat: "no-repeat",
         backgroundPosition: "static",
         height: "100vh",
+        marginBottom: "3%",
       }}
     >
       <div className="flex justify-center ">
@@ -165,7 +166,7 @@ function GitSearch() {
 
       {GitSearch && (
         <div
-          className="bg-orange-400 p-5 flex mt-20 ml-80 mr-80 rounded-3xl"
+          className="bg-orange-400 p-5 flex mt-10 ml-80 mr-80 rounded-3xl"
           style={parentDivStyle}
         >
           <img
@@ -174,13 +175,11 @@ function GitSearch() {
             alt="profile"
             src={GitSearch.avatar_url}
           />
-          <div className=" p-5 mt-3">
+          <div className="text-center p-5">
             <h1 className="text-4xl font-semibold">{GitSearch.name}</h1>
             <br />
             <p className="text-base">{GitSearch.bio}</p> <br />
-            <p className="text-base flex items-center">
-              {GitSearch.location}
-            </p>{" "}
+            <p className="text-base items-center">{GitSearch.location}</p>{" "}
             <br />
             <p className="text-base">
               Repos: {GitSearch.public_repos} &nbsp;&nbsp;&nbsp;&nbsp;
